471370f20080466e2ad5e0593294b907794b8b52
Daniil Goncharov <brovar002@gmail.com>
2023-01-15 14:01:16 +0300

    feat: add json auth parser

---
+++ app/src/main/java/by/goncharov/homestart/custom/JsonObjectRequestAuth.kt
@@ -0,0 +1,23 @@
+package by.goncharov.homestart.custom
+
+import android.util.Base64
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import by.goncharov.homestart.helpers.DeviceSecrets
+import org.json.JSONObject
+
+class JsonObjectRequestAuth(
+    method: Int,
+    url: String,
+    private val secrets: DeviceSecrets,
+    jsonRequest: JSONObject?,
+    listener: Response.Listener<JSONObject>,
+    errorListener: Response.ErrorListener
+) : JsonObjectRequest(method, url, jsonRequest, listener, errorListener) {
+
+    override fun getHeaders(): MutableMap<String, String> {
+        val params = HashMap<String, String>()
+        params["Authorization"] = "Basic " + Base64.encodeToString(("${secrets.username}:${secrets.password}").toByteArray(), Base64.NO_WRAP)
+        return params
+    }
+}
