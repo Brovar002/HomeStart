563facac1475a0b8f1c49830cc99ae7d927144a2
Daniil Goncharov <brovar002@gmail.com>
2023-01-16 11:44:44 +0300

    feat: add devices

---
+++ app/src/main/java/by/goncharov/homestart/data/ListViewItem.kt
@@ -0,0 +1,12 @@
+package by.goncharov.homestart.data
+
+class ListViewItem(
+    title: String = "",
+    summary: String = "",
+    hidden: String = "",
+    icon: Int = 0,
+    var state: Boolean? = null
+) : SimpleListItem(title, summary, hidden, icon) {
+    override fun toString(): String =
+        "title: $title, summary: $summary, hidden: $hidden, state: $state"
+}
---
+++ app/src/main/java/by/goncharov/homestart/data/SimpleListItem.kt
@@ -0,0 +1,8 @@
+package by.goncharov.homestart.data
+
+open class SimpleListItem(
+    var title: String = "",
+    var summary: String = "",
+    var hidden: String = "",
+    var icon: Int = 0
+)
