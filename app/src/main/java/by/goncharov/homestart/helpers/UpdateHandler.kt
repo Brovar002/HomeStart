2e8c01c263c0513f30d49b4a1d20349f22118db4
Daniil Goncharov <brovar002@gmail.com>
2023-02-26 21:20:03 +0300

    feat: add Update Handler

---
+++ app/src/main/java/by/goncharov/homestart/helpers/UpdateHandler.kt
@@ -0,0 +1,32 @@
+package by.goncharov.homestart.helpers
+
+import android.os.Handler
+import android.os.Looper
+
+class UpdateHandler : Handler(Looper.getMainLooper()) {
+
+    companion object {
+        private const val UPDATE_DELAY = 1000L
+    }
+
+    var running: Boolean = false; private set
+
+    fun setUpdateFunction(function: () -> Unit) {
+        removeCallbacksAndMessages(null)
+        postDelayed(
+            object : Runnable {
+                override fun run() {
+                    function()
+                    postDelayed(this, UPDATE_DELAY)
+                }
+            },
+            0,
+        )
+        running = true
+    }
+
+    fun stop() {
+        running = false
+        removeCallbacksAndMessages(null)
+    }
+}
